e272680670fbd7f7e78793ea0d6de7a218d9c949
Dayvison Cavalcanti <dayvisonpaiva8@gmail.com>
2025-05-11 22:13:28 -0300

    criando o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { QuizModule } from './quiz/quiz.module';
 import { QuestionModule } from './question/question.module';
+import {Question} from './question/question.entity'
+import {Quiz} from './quiz/quiz.entity'
+import {Result} from './result/result.entity'
+import {User} from './user/user.entity'
 
 @Module({
   imports: [
@@ -11,10 +15,11 @@ import { QuestionModule } from './question/question.module';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: 'senha',  // Substitua pela senha do seu banco
+      password: 'D4YV1S0N39',  // Substitua pela senha do seu banco
       database: 'quiz_app',
-      entities: [],
+      entities: [Question, Quiz, Result, User ],
       synchronize: true,  // Cuidado, em produção, use migrations!
+        logging: true,  // Ativa o log para ver as consultas SQL
     }),
     UserModule,
     QuizModule,
